5304ec509715dc0ea300ef07d9ce3f997f587c8b
olazy11 <olazy11@gmail.com>
2023-04-01 15:04:04 +0200

    the egine specs has been added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         audi.color ="Black";
         audi.model ="A3";
         audi.power ="5000cc";
+
+        Engine x = new Engine();
+        x.cylinders ="8";
+        x.size ="29484";
+        x.weight="84765";
+
     }
 }
